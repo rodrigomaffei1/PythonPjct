e01641c6652cf44a27a2dfa6fd24c7dd56b7d7ad
zero <rodrigo.maffeis17@gmail.com>
2024-05-03 06:11:51 -0300

    Add Comentarios no codigo

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,19 +1,21 @@
 import random
 import string
 
+# Funcao para gerar uma senha aleatoria
 def gerar_senha(tamanho=12):
-    caracteres = string.ascii_letters + string.digits
-    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    caracteres = string.ascii_letters + string.digits # Define os caracteres a serem usandos na senha
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho)) # Gera a senha concatenando caracteres aleatorios
     return senha
 
+# Funcao principal
 def main():
     while True:
-        tamanho = int(input("Digite o tamanho da senha desejada: "))
-        senha = gerar_senha(tamanho)
-        print("Senha Gerada:", senha)
+        tamanho = int(input("Digite o tamanho da senha desejada: ")) # Solicita o tamanho da senha Desejada
+        senha = gerar_senha(tamanho) # Gera a senha com o tamanho especificado
+        print("Senha Gerada:", senha) # Exibe a senha gerada
 
-        continuar = input("Deseja gerar outra senha? (s/n) ")
-        if continuar.lower() != 's':
+        continuar = input("Deseja gerar outra senha? (s/n) ") # Pergunta se deseja gerar outra senha
+        if continuar.lower() != 's': 
             break
 
     print("Programa encerrado.")
