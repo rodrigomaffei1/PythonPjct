3cc1608c5224be2e4c2d96019b6e0b8452035696
zero <rodrigo.maffeis17@gmail.com>
2024-05-03 18:26:05 -0300

    upando resumo de aula

---
+++ ExpressionsClass.py
@@ -0,0 +1,15 @@
+a = 12
+b = 3
+
+print(a + b)    # 15
+print(a - b)    # 9
+print(a * b)    # 36
+print(a / b)    # 4.0
+print(a // b)   # 4 Divisão de números inteiros, arredondada para baixo em direção ao infinito negativo
+print(a % b)    # 0 Módulo: o resto após a divisão de números inteiros
+
+print()
+
+for i in range(1, a // b):
+    print(i)
+
