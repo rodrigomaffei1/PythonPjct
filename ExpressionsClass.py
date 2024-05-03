0bb4b5775c94f6988cd6670f0b2ad6431c036da1
zero <rodrigo.maffeis17@gmail.com>
2024-05-03 18:52:05 -0300

    add mais exemplos
    Atividade Expressions

--- ExpressionsClass.py
+++ ExpressionsClass.py
@@ -8,8 +8,11 @@ print(a / b)    # 4.0
 print(a // b)   # 4 Divisão de números inteiros, arredondada para baixo em direção ao infinito negativo
 print(a % b)    # 0 Módulo: o resto após a divisão de números inteiros
 
-print()
+print("\nPrimeiro loop de 1 ate soma de a + b")
+for i in range(1, a + b):
+    print(i)
 
-for i in range(1, a // b):
+print("\nSegundo loop de b ate a")
+for i in range(b, a):
     print(i)
 
