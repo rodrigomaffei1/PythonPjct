9930fc1f3c390ed49ae445f76d50e876152bff19
zero <rodrigo.maffeis17@gmail.com>
2024-05-03 06:00:04 -0300

    PasswordGenerator
    primeiro codigo

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,1 +1,22 @@
-import 
+import random
+import string
+
+def gerar_senha(tamanho=12):
+    caracteres = string.ascii_letters + string.digits
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+def main():
+    while True:
+        tamanho = int(input("Digite o tamanho da senha desejada: "))
+        senha = gerar_senha(tamanho)
+        print("Senha Gerada:", senha)
+
+        continuar = input("Deseja gerar outra senha? (s/n) ")
+        if continuar.lower() != 's':
+            break
+
+    print("Programa encerrado.")
+
+if __name__ == "main__":
+    main()
