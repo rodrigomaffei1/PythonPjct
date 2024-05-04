8a14b0a5b561ba4747e549663628793f189fbdbe
zero <rodrigo.maffeis17@gmail.com>
2024-05-04 02:35:58 -0300

    Exercicio python with price

---
+++ Exercise4.py
@@ -0,0 +1,11 @@
+#Exercise
+
+#You have a shop selling buns for $2.40 each.  A customer comes in with $15, and would like to buy as many buns as possible.
+#Complete the code to calculate how many buns the customer can afford.
+# Note: Your customer won't be happy if you try to sell them part of a bun.
+
+bun_price = 2.40
+money = 15
+
+print(money // bun_price)
+
